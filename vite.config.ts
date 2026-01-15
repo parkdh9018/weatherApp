cd6e9d3d486dc6565e8824d40e7862b1195360d8
parkdh9018 <parkdh9018@naver.com>
2026-01-15 21:57:37 +0900

    chore: 카카오 api를 위한 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { env } from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "html-transform",
+      transformIndexHtml(html) {
+        return html.replace(
+          "%VITE_KAKAO_APP_KEY%",
+          env.VITE_KAKAO_APP_KEY || ""
+        );
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
